4825fdd542e5e66abbcf3933f700d3e4428f81e4
Александр Куваев <vinatorul@yandex-team.ru>
2024-12-14 20:37:21 +0300

    wip

--- src/ping.cpp
+++ src/ping.cpp
@@ -2,18 +2,16 @@
 
 #include <fmt/format.h>
 
-#include <userver/server/handlers/http_handler_base.hpp>
-
 namespace service_template {
-  
-std::string Ping::HandleRequestThrow(
-      const userver::server::http::HttpRequest &request,
-      userver::server::request::RequestContext &) const override {
-    
-    counter_++;
 
-    return std::to_string(counter_);
-  }
+std::string Ping::HandleRequestThrow(
+    const userver::server::http::HttpRequest &request,
+    userver::server::request::RequestContext &) const {
+  
+  counter_++;
+
+  return std::to_string(counter_);
+}
 
 void AppendPing(userver::components::ComponentList &component_list) {
   component_list.Append<Ping>();
